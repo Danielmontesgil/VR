5dc1336450e9c3087ca16f862a97e20f9386f6a0
Daniel Montes <daniel.montes@upb.edu.co>
2017-09-12 14:51:00 -0500

    donde ibamos

---
+++ Assets/_Game/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float velocity;
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private Rigidbody rb;
+
+    public void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = camera.transform.forward * velocity * Time.deltaTime;
+        }
+    }
+}
