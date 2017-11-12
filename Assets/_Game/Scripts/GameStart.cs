c24f6eaffb88ff8fe79ee2eb337002ce2006b48d
Danielmontesgil <daniel.montes@upb.edu.co>
2017-11-12 11:46:35 -0500

    audio instructions

--- Assets/_Game/Scripts/GameStart.cs
+++ Assets/_Game/Scripts/GameStart.cs
@@ -6,20 +6,53 @@ public class GameStart : MonoBehaviour {
     private GameObject player;
     [SerializeField]
     private Canvas perma;
-
+    [SerializeField]
+    private AudioClip[] audioInstructions;
+    private GvrAudioSource instructions;
+    
     private float time;
 
 	void Start () {
         player.GetComponent<PlayerMovement>().enabled = false;
+        instructions = GetComponent<GvrAudioSource>();
+        instructions.clip = audioInstructions[0];
+        instructions.Play();
     }
 	
 	void Update () {
         time += Time.deltaTime;
-        if (time >= 10)
+        if (instructions.clip==audioInstructions[0] && time >= 6)
         {
-            perma.gameObject.SetActive(true);
-            player.GetComponent<PlayerMovement>().enabled = true;
-            gameObject.SetActive(false);
+            instructions.clip = audioInstructions[1];
+            instructions.Play();
+            Debug.Log(instructions.clip);
+            Debug.Log(time);
         }
-	}
+        if (instructions.clip == audioInstructions[1] && time >= 14)
+        {
+            instructions.clip = audioInstructions[2];
+            instructions.Play();
+        }
+        if (instructions.clip == audioInstructions[2] && time >= 20.5)
+        {
+            instructions.clip = audioInstructions[3];
+            instructions.Play();
+        }
+        if (instructions.clip == audioInstructions[3] && time >= 27.5)
+        {
+            instructions.clip = audioInstructions[4];
+            instructions.Play();
+        }
+        if (instructions.clip == audioInstructions[4] && time >= 34)
+        {
+            instructions.clip = audioInstructions[5];
+            instructions.Play();
+        }
+        if (instructions.clip == audioInstructions[5] && time >= 40)
+        {
+            player.GetComponent<PlayerMovement>().enabled = true;
+            perma.gameObject.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
 }
