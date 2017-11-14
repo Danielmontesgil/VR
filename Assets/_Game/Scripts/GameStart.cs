20ee34e139bdd7ce31bba4bc5ac240ad0e8081dd
Danielmontesgil <daniel.montes@upb.edu.co>
2017-11-14 18:37:34 -0500

    final instructions

--- Assets/_Game/Scripts/GameStart.cs
+++ Assets/_Game/Scripts/GameStart.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStart : MonoBehaviour {
 
@@ -8,6 +9,16 @@ public class GameStart : MonoBehaviour {
     private Canvas perma;
     [SerializeField]
     private AudioClip[] audioInstructions;
+    [SerializeField]
+    private Text textCanvas;
+    [SerializeField]
+    private Image imageCanvas;
+    [SerializeField]
+    private Sprite[] imageInstructions;
+    [SerializeField]
+    private Image arrow;
+    [SerializeField]
+    private GvrAudioSource water;
     private GvrAudioSource instructions;
     
     private float time;
@@ -17,6 +28,9 @@ public class GameStart : MonoBehaviour {
         instructions = GetComponent<GvrAudioSource>();
         instructions.clip = audioInstructions[0];
         instructions.Play();
+        textCanvas.text = "Can you hear me? Hi, I'm Begonia, I speak to you from the base";
+        imageCanvas.gameObject.SetActive(false);
+        arrow.gameObject.SetActive(false);
     }
 	
 	void Update () {
@@ -25,30 +39,59 @@ public class GameStart : MonoBehaviour {
         {
             instructions.clip = audioInstructions[1];
             instructions.Play();
-            Debug.Log(instructions.clip);
-            Debug.Log(time);
+            textCanvas.text = "You are in a cave to look the biggest treasure never seen, to move use control's pad";
+            imageCanvas.gameObject.SetActive(true);
+            imageCanvas.sprite = imageInstructions[0];
+            arrow.gameObject.SetActive(true);
         }
         if (instructions.clip == audioInstructions[1] && time >= 14)
         {
             instructions.clip = audioInstructions[2];
             instructions.Play();
+            textCanvas.text = "According to the legend you should follow the water drops' sound to find the exit";
+            imageCanvas.gameObject.SetActive(false);
+            arrow.gameObject.SetActive(false);
         }
         if (instructions.clip == audioInstructions[2] && time >= 20.5)
         {
-            instructions.clip = audioInstructions[3];
+            instructions.clip = audioInstructions[6];
+            instructions.gainDb = 9f;
             instructions.Play();
         }
-        if (instructions.clip == audioInstructions[3] && time >= 27.5)
+        if (instructions.clip == audioInstructions[6] && time >= 22.5)
+        {
+            instructions.clip = audioInstructions[3];
+            instructions.gainDb = 0f;
+            instructions.Play();
+            textCanvas.text = "But don't forget you can't go out without the six treasure chest";
+            imageCanvas.gameObject.SetActive(true);
+            imageCanvas.sprite = imageInstructions[1];
+            imageCanvas.rectTransform.sizeDelta = new Vector2(1f, 1.22f);
+        }
+        if (instructions.clip == audioInstructions[3] && time >= 29.5)
         {
             instructions.clip = audioInstructions[4];
             instructions.Play();
+            textCanvas.text = "And take care of the poison, after a few time it will be letal";
+            imageCanvas.gameObject.SetActive(true);
+            imageCanvas.sprite = imageInstructions[2];
+            imageCanvas.rectTransform.sizeDelta = new Vector2(1f, 1.29f);
         }
-        if (instructions.clip == audioInstructions[4] && time >= 34)
+        if (instructions.clip == audioInstructions[4] && time >= 36)
         {
             instructions.clip = audioInstructions[5];
             instructions.Play();
+            textCanvas.text = "Unfortunately we don't know who is there with you... Good luck!!";
+            imageCanvas.gameObject.SetActive(false);
         }
-        if (instructions.clip == audioInstructions[5] && time >= 40)
+        if (instructions.clip == audioInstructions[5] && time >= 42)
+        {
+            player.GetComponent<PlayerMovement>().enabled = true;
+            perma.gameObject.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+
+        if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKey(KeyCode.F))
         {
             player.GetComponent<PlayerMovement>().enabled = true;
             perma.gameObject.SetActive(true);
